154eb1d09ac175847156324a3f364ba676b29c99
MXuanZero <MXuan_0@163.com>
2024-06-30 09:05:30 +0800

    fead: GD32F303-Porject-GNU-CMake

---
+++ GD32/GD32F303-Porject-GNU-CMake/src/core/gd32f30x_it.c
@@ -0,0 +1,78 @@
+#include "gd32f30x_it.h"
+#include "systick.h"
+
+/**
+ * @brief this function handles BusFault exception
+ **/
+void NMI_Handler(void)
+{
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void HardFault_Handler(void)
+{
+    /* if Hard Fault exception occurs, go to infinite loop */
+    while (1){
+    }
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void MemManage_Handler(void)
+{
+    /* if Memory Manage exception occurs, go to infinite loop */
+    while (1){
+    }
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void BusFault_Handler(void)
+{
+    /* if Bus Fault exception occurs, go to infinite loop */
+    while (1){
+    }
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void UsageFault_Handler(void)
+{
+    /* if Usage Fault exception occurs, go to infinite loop */
+    while (1){
+    }
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void SVC_Handler(void)
+{
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void PendSV_Handler(void)
+{
+}
+
+/**
+ * @brief this function handles BusFault exception
+ */
+void SysTick_Handler(void)
+{
+    delay_decrement();
+}
